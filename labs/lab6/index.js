6204b0ffa98a24277cc35ed0ebce767e5d6e8206
eduardo-hdez <ehdez2004@gmail.com>
2026-02-19 21:37:36 -0600

    [Feat] add verify password function

---
+++ labs/lab6/index.js
@@ -0,0 +1,12 @@
+const inputOne = document.getElementById("first-filter");
+const inputTwo = document.getElementById("second-filter");
+const confirmButton = document.getElementById("button-confirm");
+const output = document.getElementById("output");
+
+confirmButton.addEventListener("click", () => {
+  if (inputOne.value === inputTwo.value) {
+    output.innerHTML = "<p>Password Verified!</p>";
+  } else {
+    output.innerHTML = "<p>Wrong Match</p>";
+  }
+});
