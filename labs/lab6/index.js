a7fb4d62cf2fd2f004e2cde748e2eaf8e76ede5a
eduardo-hdez <ehdez2004@gmail.com>
2026-02-22 13:20:07 -0600

    [Style] add style to index.css

--- labs/lab6/index.js
+++ labs/lab6/index.js
@@ -4,7 +4,9 @@ const confirmButton = document.getElementById("button-confirm");
 const output = document.getElementById("output");
 
 confirmButton.addEventListener("click", () => {
-  if (inputOne.value === inputTwo.value) {
+  if (inputOne.value === "" || inputTwo.value === "") {
+    output.innerHTML = "<p>Insert valid characters</p>";
+  } else if (inputOne.value === inputTwo.value) {
     output.innerHTML = "<p>Password Verified!</p>";
   } else {
     output.innerHTML = "<p>Wrong Match</p>";
