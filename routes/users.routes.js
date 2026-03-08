3190941a7844973ffa24506b19bb620801860c9c
eduardo-hdez <ehdez2004@gmail.com>
2026-03-07 18:31:41 -0600

    [Feat] add controller and model for users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,36 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-// Data
-const users = [
-  {
-    name: "eduardo-hdez",
-    description: "Computer Science and Technology",
-  },
-  {
-    name: "elias-franco",
-    description: "Robotics and Digital Systems Engineering",
-  },
-];
+const usersController = require("../controllers/users.controller");
 
-// Middleware
-router.use((request, response, next) => {
-  console.log("¡I'm a middleware!");
-  next();
-});
-
-router.get("/new", (request, response, next) => {
-  response.render("users/new", { title: "Create New User" });
-});
-
-router.post("/new", (request, response, next) => {
-  users.push(request.body);
-  response.redirect("/users");
-});
-
-router.use((request, response, next) => {
-  console.log("Another middleware!");
-  response.render("users/index", { title: "Users", users: users });
-});
+router.get("/", usersController.getUsers);
+router.get("/new", usersController.getNewUser);
+router.post("/new", usersController.postNewUser);
 
 module.exports = router;
