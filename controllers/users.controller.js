8f5fc3a2ed7fec239b0e9568e1933e5ebea01a49
eduardo-hdez <ehdez2004@gmail.com>
2026-03-07 18:31:41 -0600

    [Feat] add controller and model for users

---
+++ controllers/users.controller.js
@@ -0,0 +1,18 @@
+const Users = require("../models/users.model");
+
+const path = require("path");
+
+exports.getUsers = (request, response, next) => {
+  const users = Users.fetchAll();
+  response.render("users/index", { users: users, title: "Users" });
+};
+
+exports.getNewUser = (request, response, next) => {
+  response.render("users/new", { title: "New User" });
+};
+
+exports.postNewUser = (request, response, next) => {
+  const user = new Users(request.body.name, request.body.description);
+  user.save();
+  response.redirect("/users");
+};
