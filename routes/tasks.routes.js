f25ab8b2c143b50c6f6ca7404878cff7db4b1a13
eduardo-hdez <ehdez2004@gmail.com>
2026-03-14 21:13:36 -0600

    [Feat] add new routes

--- routes/tasks.routes.js
+++ routes/tasks.routes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 const tasksController = require("../controllers/tasks.controller");
 
-router.get("/", tasksController.getAllTasks);
-router.get("/new-task", tasksController.getNewTask);
-router.post("/new-task", tasksController.postNewTask);
-router.get("/:id/edit-task", tasksController.getEditTask);
-router.post("/:id/edit-task", tasksController.postEditTask);
-router.get("/:id", tasksController.getAllTasks);
+const isAuth = require("../util/isAuth");
+const canCreateTask = require("../util/canCreateTask");
+const canViewOwnTasks = require("../util/canViewOwnTasks");
+const canUpdateOwnTask = require("../util/canUpdateOwnTask");
+const canDeleteOwnTask = require("../util/canDeleteOwnTask");
+
+router.get("/", isAuth, canViewOwnTasks, tasksController.getAllTasks);
+router.get("/new-task", isAuth, canCreateTask, tasksController.getNewTask);
+router.post("/new-task", isAuth, canCreateTask, tasksController.postNewTask);
+router.get("/:id/edit-task", isAuth, canUpdateOwnTask, tasksController.getEditTask);
+router.post("/:id/edit-task", isAuth, canUpdateOwnTask, tasksController.postEditTask);
+router.post("/:id/delete", isAuth, canDeleteOwnTask, tasksController.postDeleteTask);
 
 module.exports = router;
