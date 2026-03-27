b408d9a58a9396f76bb3309d63be4fe69f1ce892
eduardo-hdez <ehdez2004@gmail.com>
2026-03-27 17:31:44 -0600

    [Chore] remove node_modules and modify .gitignore

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const usersController = require("../../controllers/users.controller");
+const usersController = require("../controllers/users.controller");
 
 const isAuth = require("../util/isAuth");
 const canViewUsers = require("../util/canViewUsers");
